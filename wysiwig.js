8499f1d5375e4748df362f29c7e96f2fab2d1f8d
Kristin Caron <krissycaron@gmail.com>
2017-03-20 21:32:51 -0500

    got the p tag of the bio targeted and ready to write the function to mirror change the text

--- wysiwig.js
+++ wysiwig.js
@@ -1,6 +1,7 @@
-// Creating an Array
+// Create an array of objects that represents famous people
 var outputEl = document.getElementById("container");
-var inputTextFromDOM = document.getElementById("inputEl")
+var inputTextFromDOM = document.getElementById("inputEl");
+var selectedBio; 
 var famousPeople = [
 		{
 		  title: "Singer",
@@ -34,6 +35,8 @@ var famousPeople = [
 		}
 ];
 
+// Create a DOM element for each of the objects inside the container. Style your person elements however you like.
+
 function printPeopleToDom () {
 	for(var i=0; i<famousPeople.length; i++){
 		outputEl.innerHTML += `<div id="person-${i}" class="person">
@@ -54,30 +57,42 @@ function printPeopleToDom () {
 }
 
 function selectedFamousPerson(e) {
-	console.log(e);
 	var peopleCards = document.getElementsByClassName("person");
   for (var i = 0; i < famousPeople.length; i++) {
   	peopleCards[i].classList.remove("selected")
   }
-
-  	if (e.target.classList.contains("person")) {
+	if (e.target.classList.contains("person")) {
   		e.target.classList.add("selected");
+  		selectedBio = e.target.childNodes[3].childNodes[1];
 	} else if (e.target.classList.contains("child")) {
 		e.target.parentNode.classList.add("selected");
+		selectedBio = e.target.parentNode.childNodes[3].childNodes[1];
 	} else if (e.target.classList.contains("grandChild")) {
 		e.target.parentNode.parentNode.classList.add("selected");
+		selectedBio = e.target.parentNode.parentNode.childNodes[3].childNodes[1];
 	} 
 }
+// When you click on one of the person elements, the text input should immediately gain focus so that you can start typing.
+// When there is a highlighted person element, and you begin typing in the input box, the person's biography should be immediately bound to what you are typing, letter by letter.
 
-
+// function editBio() {
+// 	for (var k=0; k<famousPeople.length; k++) {
+// 		if (famousPeople[i] === "selected"){
+// 			console.log(famousPeople);
+// 	}	
+// 			// allow changes to be made to "bio" by replacing with the input text. 
+// }
 
 outputEl.addEventListener("click", selectedFamousPerson);
 
+// When you press the enter/return key when typing in the input field, then the content of the input field should immediately be blank.
 
-var mirrorText = function (event) {
-	outputEl.innerHTML += event.key;
-}
-inputTextFromDOM.addEventListener("keypress", mirrorText);
+
+// var mirrorText = function (event) {
+// 	outputEl.innerHTML += event.key;
+// }
+// inputTextFromDOM.addEventListener("keypress", mirrorText);
+
 
 
 printPeopleToDom();
